8c98154af5fa7e771c2d4fbeae447fe8f0d31d24
vvnzheng <91556534+vvnzheng@users.noreply.github.com>
2021-10-13 17:38:08 -0700

    Finished Lab4 website

---
+++ lab4/js/lab.js
@@ -0,0 +1,14 @@
+// Summary.
+// Javascript pseudocode comments for code.
+// Description.
+// This file contains pseudocode comments for coding 'mad lib'.
+// @link art101/lab4/js/lab.js
+// @file lab.js
+// @author Vivian Zheng
+// @since 10.13.2021
+//For our computer task, we decided to pick ‘mad lib.’
+  //Set up story template with empty variables representing ‘blanks’
+    //Loop through variables in a list
+      //Have the user input words for the madlib
+      //Assign user input to the empty variables
+    //Print final story with variables’ data replaced with user input words
