0164fa6686830608111361d3c60021ba1e34898c
vvnzheng <91556534+vvnzheng@users.noreply.github.com>
2021-10-18 19:09:46 -0700

    Finished Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,32 @@
+/*
+ * Author: Vivian Zheng <vizheng@ucsc.edu>
+ * Created: October 18 2021
+ * License: Public Domain
+*/
+
+//Variables
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year = 1995;
+str = new String()
+
+//calculation
+age = 2021 - year;
+
+//result
+document.writeln("Make: ")
+str = (make + "<br>");
+document.writeln(str.fontcolor("blue"));
+document.writeln("Model: ")
+str = (model + "<br>");
+document.writeln(str.fontcolor("blue"));
+document.writeln("Color: ")
+str = (color + "<br>");
+document.writeln(str.fontcolor("blue"));
+document.writeln("Year: ")
+str = (year + "<br>");
+document.writeln(str.fontcolor("blue"));
+document.writeln("Age: ")
+str = (age + " years<br>");
+document.writeln(str.fontcolor("blue"));
