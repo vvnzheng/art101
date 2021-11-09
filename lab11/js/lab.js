a69c4dd144f40bef707d1a02ec4d4316fcdbd844
vvnzheng <91556534+vvnzheng@users.noreply.github.com>
2021-11-08 19:29:43 -0800

    finished lab11

---
+++ lab11/js/lab.js
@@ -0,0 +1,14 @@
+/*
+ * Author: Vivian Zheng
+ * Created: November 8 2021
+ * License: Public Domain
+*/
+$("#challengeButton").click(function(){
+  $("#first").toggleClass("special")
+})
+$("#problemButton").click(function(){
+  $("#second").toggleClass("special")
+})
+$("#resultButton").click(function(){
+  $("#third").toggleClass("special")
+})
