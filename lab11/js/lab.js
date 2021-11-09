d8526c14271c072884b5c61ae1824966b832356a
vvnzheng <91556534+vvnzheng@users.noreply.github.com>
2021-11-08 20:00:30 -0800

    added bonus task for lab11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -3,12 +3,23 @@
  * Created: November 8 2021
  * License: Public Domain
 */
+//toggle the button in the challenge section to bold and purple large font
 $("#challengeButton").click(function(){
-  $("#first").toggleClass("special")
+  $("#first").toggleClass("special");
 })
+//toggle the button in the problem section to bold and purple large font
 $("#problemButton").click(function(){
-  $("#second").toggleClass("special")
+  $("#second").toggleClass("special");
 })
+//toggle the button in the result section to bold and purple large font
 $("#resultButton").click(function(){
-  $("#third").toggleClass("special")
+  $("#third").toggleClass("special");
+})
+//toggle the color of button2
+$("#button1").click(function(){
+  $("#button2").toggleClass("color1");
+})
+//toggle the color of button1
+$("#button2").click(function(){
+  $("#button1").toggleClass("color2");
 })
