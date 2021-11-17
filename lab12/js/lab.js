524778a6f0275dfc8c3fad94b8ad22751825ea8c
vvnzheng <vvnezheng@gmail.com>
2021-11-16 19:24:08 -0800

    Changed and finished lab12

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -3,28 +3,35 @@
  * Created: November 15 2021
  * License: Public Domain
 */
-function sortingHat(str){
-  length = 0;
-  while(str){
-    length++;
+//Created function sortingHat to sort names
+function sortingHat(name){
+  //take the length of the given name and mod it by 4
+  mod = name.length%4;
+  //If the result is 0, return "Gryffindor"
+  if(mod == 0){
+    return "Gryffindor"
   }
-  mod = length%4;
-if(mod == 0){
-  return "Gryffindor"
+  //If the result is 1, return "Ravenclaw"
+  if(mod == 1){
+    return "Ravenclaw"
+  }
+  //If the result is 2, return "Slytherin"
+  if(mod == 2){
+    return "Slytherin"
+  }
+  //If the result is 3, return "Hufflepuff"
+  if(mod == 3){
+    return "Hufflepuff"
+  }
 }
-if(mod == 1){
-  return "Ravenclaw"
-}
-if(mod == 2){
-  return "Slytherin"
-}
-if(mod == 3){
-  return "Hufflepuff"
-}
-}
-
+//When you click the button, sort the name
 $("#button").click(function(){
+  //Take in the input and store it in a variable
+  var field = document.getElementById("input");
+  var name = field.value;
+  //Insert it as a parameter to the sortingHat function
   house = sortingHat(name);
+  //Output the result into a sentence
   var headingEl = document.getElementById("output");
   headingEl.innerHTML = "The Sorting Hat has sorted you into " + house + "!";
 })
