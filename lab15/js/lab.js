23ff67f6667de78ab27f1048f2be8d9c6c44cd32
vvnzheng <91556534+vvnzheng@users.noreply.github.com>
2021-11-29 22:35:28 -0800

    finished lab15

---
+++ lab15/js/lab.js
@@ -0,0 +1,27 @@
+/*
+ * Author: Vivian Zheng
+ * Created: November 29 2021
+ * License: Public Domain
+*/
+var my_url = "http:/numbersapi.com/7/math"
+$("#activate").click(function(){
+$.ajax({
+  //endpoint
+  url: my_url,
+  //data to send
+  //data: { id: 123},
+  //GET request
+  type: "GET",
+  //type we get back
+  //dataType: "json",
+})
+//if success, pass data back
+.done(function(data){
+  console.log("Success: ", data);
+  $("#output").html(data);
+})
+//if fail, send error
+.fail(function(request, error){
+  console.log(request, error);
+})
+})
